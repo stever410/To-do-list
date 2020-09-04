4852137917cf4d4ce36d2edf84531922059278ae
stever410 <54258769+stever410@users.noreply.github.com>
2020-09-04 18:19:33 +0700

    Update ui, add button to to-do item

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 let toDoList = [];
 const list = document.getElementById("to-do-list");
 
-document.getElementById('add-btn').addEventListener("click", (event) => {
-    let item = document.getElementById('thing-to-do').value;
-    const itemHtml = `<li class="to-do-item">${item}</li>`;
+document.getElementById("add-btn").addEventListener("click", (event) => {
+  let item = document.getElementById("thing-to-do").value;
+  const itemHtml = `
+    <li class="to-do-item">
+    ${item}
+    <span class="item-tool">
+      <i class="fas fa-minus-square fa-lg"></i>
+      <i class="fas fa-check-circle fa-lg"></i>
+    </span>
+  </li>`;
+  if (item && item !== "") {
+    toDoList.push(item);
+    list.insertAdjacentHTML("beforeend", itemHtml);
+    document.getElementById("thing-to-do").value = "";
+  }
+  console.log(toDoList);
+});
+
+document.getElementById("thing-to-do").addEventListener("keydown", (event) => {
+  if (event.keyCode === 13) {
     event.preventDefault();
-    if(item && item !== '') {
-        toDoList.push(item);
-        list.insertAdjacentHTML("beforeend", itemHtml);
-        document.getElementById('thing-to-do').value = "";
-    }
-    console.log(toDoList);
+    document.getElementById("add-btn").click();
+  }
 });
